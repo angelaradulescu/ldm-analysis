9b7fe25b5092d6281b44760dc9915329a18032c4
Angela Radulescu <angela.radulescu@gmail.com>
2022-04-04 15:08:25 -0400

    commit old changes

--- ldm_mixed_effects.R
+++ ldm_mixed_effects.R
@@ -2,11 +2,13 @@ library(lattice)
 library(lme4)
 library(lmerTest)
 library(psycho)
+library(sjPlot)  # for plotting lmer and glmer mods
+library(emmeans) # for post-hoc tests
 
 getwd()
 
 data <- read.csv('/Users/angelaradulescu/Dropbox/NYU/Research/LDM/ldm-analysis/ProcessedData/Feedback_Processed_CombinedBehavioralEyetrackingData.csv')
-print(data)
+head(data, 10)
 
 ## Model. 
 m1 <- lmer(data = data, formula = Entropy ~ Age*LearnedFeat + (1|Subj))
@@ -15,4 +17,6 @@ summary(m1)
 ## Omnibus test. 
 car::Anova(m1, type = '3')
 
+## Plot effects.
+sjPlot::plot_model(m1, type = 'int')
 
